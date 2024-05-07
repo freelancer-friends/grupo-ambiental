fd7d7711b3d03fd2fe02fa225d47029309d3eda7
Lucas Rocha <lucasbezerrar@gmail.com>
2024-05-07 15:57:49 -0300

    Resolvido problema da função js

--- assets/js/modules/menu-mobile.js
+++ assets/js/modules/menu-mobile.js
@@ -1,10 +1,11 @@
 export default function initMenuMobile() {
   const btnMobile = document.getElementById('btn-mobile');
 
-  function toggleMenu() {
-    const nav = document.querySelector('#nav');
-    nav.classList.toggle('active');
+  if(btnMobile) {
+    function toggleMenu() {
+      const nav = document.querySelector('#nav');
+      nav.classList.toggle('active');
+    }
+    btnMobile.addEventListener('click', toggleMenu);
   }
-
-  btnMobile.addEventListener('click', toggleMenu);
 }
